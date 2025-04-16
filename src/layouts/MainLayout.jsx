54aa665d1c673ba0de9c5fe31b1a30c770749e73
Dmytro <dmitriylishtvan@gmail.com>
2025-04-16 10:43:23 +0300

    added left navbar

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,24 +1,50 @@
-// src/layouts/MainLayout.tsx
-import { AppBar, Box, CssBaseline, Drawer, Toolbar, Typography, List, ListItem, ListItemText } from '@mui/material';
-import {Outlet, Link, useLocation } from 'react-router';
-import {Header} from "../components/Header";
+import {Outlet} from 'react-router';
+// import {Header} from "../components/Header";
 import Container from "@mui/material/Container";
 import {Footer} from "../components/Footer.jsx";
+// import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+// import { PageContainer } from '@toolpad/core/PageContainer';
+import { ReactRouterAppProvider } from '@toolpad/core/react-router';
+import {
+  Home,
+  Autorenew,
+  AttachMoney
+} from '@mui/icons-material';
+
+const NAVIGATION = [
+  // {
+  //
+  //   title: 'ImportFlow',
+  //   icon: <Home />,
+  // },  
+  {
+    segment: 'integrations',
+    title: 'Integrations',
+    icon: <Autorenew />,
+  },  
+  {
+    segment: 'invoices',
+    title: 'Invoices',
+    icon: <AttachMoney />,
+  },
+];
 
 const MainLayout = () => {
   return (
     <>
-      <Header></Header>
-
-      <Container
-        maxWidth="lg"
-        component="main"
-        sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4 }}
-      >
-        <Outlet/>
-      </Container>
-      
-      <Footer/>
+      <ReactRouterAppProvider navigation={NAVIGATION}>
+        <DashboardLayout branding={{homeUrl: '/', title: 'ImportFlow', logo: ''}}>
+          <Container
+            maxWidth="xl"
+            sx={{ display: 'flex', flexDirection: 'column', my: 2, gap: 4 }}
+          >
+            <Outlet/>
+          </Container>
+        </DashboardLayout>
+        {/*<Header></Header>*/}
+      </ReactRouterAppProvider>
+      {/*<Footer/>*/}
     </>
   );
 }
